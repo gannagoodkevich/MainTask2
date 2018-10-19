c6a6d4ecb2e43a204465773ee8006b93390b280f
gannagoodkevich <34305372+gannagoodkevich@users.noreply.github.com>
2018-10-19 23:14:58 +0300

    Delete Func.java

--- HT2/src/Func.java
+++
@@ -1,9 +0,0 @@
-
-public class Func {
-	public boolean check (int a, int b, int c) {
-		boolean var1 = (a==b);
-		boolean var2 = (b==c);
-		boolean answer = ((var1==true)&&(var2==true));		
-		return answer;
-	}
-}
